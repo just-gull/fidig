843f58bb7a0eaab85e728dcaf110a65dac2eee5c
Sergey Petrenko <just.gull@gmail.com>
2023-02-06 12:32:02 +0300

    add OpenSSL library

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <openssl/evp.h>
+
 static void print_usage() {
   printf("usage: fidig <digest-type> <path-to-a-file>\n");
   printf(" -h, --help - this message\n");
@@ -26,5 +28,15 @@ int main(int argc, char* argv[]) {
     }
   }
 
+  OSSL_LIB_CTX *openssl_context;
+  openssl_context = OSSL_LIB_CTX_new();
+  if (openssl_context == NULL) {
+    fprintf(stderr, "OpenSSL context is NULL!\n");
+    goto cleanup;
+  }
+
+cleanup:
+  OSSL_LIB_CTX_free(openssl_context);
+  
   return EXIT_SUCCESS;
 }
