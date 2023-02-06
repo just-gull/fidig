c5ed14f97b4204bfa4f0b51fb8f8fc07e4a91b67
Sergey Petrenko <just.gull@gmail.com>
2023-02-06 11:30:34 +0300

    add usage message

--- main.c
+++ main.c
@@ -1,5 +1,30 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-int main(void) {
+static void print_usage() {
+  printf("usage: fidig <digest-type> <path-to-a-file>\n");
+  printf(" -h, --help - this message\n");
+  printf(" -l, --list - lists available digests\n");
+}
+
+static void print_digests() {
+  printf("Supported digests: ...\n");
+}
+
+int main(int argc, char* argv[]) {
+  if (!(argc == 2 || argc == 3)) {
+    print_usage();
+    return EXIT_SUCCESS;
+  } else if (argc == 2) {
+    if (strncmp(argv[1], "-l", 3) == 0 || strncmp(argv[1], "--list", 7) == 0) {
+      print_digests();
+      return EXIT_SUCCESS;
+    } else {
+      print_usage();
+      return EXIT_SUCCESS;
+    }
+  }
+
   return EXIT_SUCCESS;
 }
